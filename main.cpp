573f1f677f2ac4bfb099ca810f465d5c8ec12e7c
Thomas Kowalski <thom.kowa@gmail.com>
2018-10-09 20:56:09 +0200

    Work on the model

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
-#include "logger.hpp"
 #include "config.hpp"
+#include "logger.hpp"
 #include "model.hpp"
 
-auto main() -> int
-{
+const unsigned int teachers = 5;
+const unsigned int students = 4;
+const unsigned int durations[] = {1,2,3,4,5};
 
-    return 0;
+auto main() -> int { 
+    Solution S(teachers, students, durations);
+    return 0; 
 }
--- model.hpp
+++ model.hpp
@@ -1,48 +1,93 @@
-#include <vector>
 #include <algorithm>
+#include <ctime>
+#include <array>
+#include <iostream>
+#include <vector>
+#include <random>
+#include <map>
 
-#ifndef ORDER_TEMPLATE_IMPLEMENTATION
-class Order {
-    private:
-        const unsigned int size_;
-        unsigned int* order_;
+#ifndef MODEL_HPP
+#define MODEL_HPP
 
-    public:
-        Order(const unsigned int size) : size_(size)
-        {
-            order_ = new unsigned int[size];
-        }
+class Order
+{
+  private:
+    const unsigned int size_;
+    unsigned int *order_;
 
-        Order(const Order& from) : size_(from.size_)
-        {
-            order_ = new unsigned int[size_];
-            for(unsigned int i = 0; i < size_; i++)
-                order_[i] = from.order_[i];
-        }
+  public:
+    Order(const unsigned int size) : size_(size)
+    {
+        order_ = new unsigned int[size];
 
-        ~Order()
-        {
-            delete[] order_;
-        }
+        for (unsigned int i = 0; i < size_; i++)
+            order_[i] = i;
+    }
+
+    Order(const Order &from) : size_(from.size_)
+    {
+        order_ = new unsigned int[size_];
+        for (unsigned int i = 0; i < size_; i++)
+            order_[i] = from.order_[i];
+    }
+
+    auto shuffle()
+    {
+        std::srand(std::time(nullptr));
+
+        auto random_variable = std::rand();
+
+        
+    }
+
+    auto begin()
+    {
+        return order_;
+    }
+
+    auto end()
+    {
+        return order_ + size_;
+    }
+
+    ~Order() { delete[] order_; }
 };
-#endif
 
-Order make_order(unsigned int length)
+auto make_order(unsigned int length)
 {
     Order o(length);
+    o.shuffle();
+    return o;
 }
 
-class Solution {
-    private:
-        Order teacher_order;
-        std::vector<Order> chromosomes;
+class Solution
+{
+  private:
+    Order teacher_order;
+    std::vector<Order> chromosomes;
+    std::map<unsigned int, unsigned int> durations_;
 
-        const unsigned int teachers_;
-        const unsigned int students_;
+    const unsigned int teachers_;
+    const unsigned int students_;
 
-    public:
-        Solution(const unsigned int teachers, const unsigned int students) : teachers_(teachers), students_(students)
+  public:
+    Solution(const unsigned int teachers, const unsigned int students, const unsigned int *durations)
+        : teachers_(teachers), students_(students),
+          teacher_order(make_order(teachers))
+    {
+        for (unsigned int i = 0; i < teachers_; i++)
+            durations_.insert(i, durations[i]);
+    }
+
+    auto makespan()
+    {
+        for (const auto teacher_id : teacher_order)
         {
-            
+            std::cout << "Teacher " << teacher_id << std::endl;
+            for (const auto &student_id : chromosomes.at(teacher_id))
+                std::cout << "\tStudent " << student_id << std::endl;
         }
+    }
 };
+
+#endif
